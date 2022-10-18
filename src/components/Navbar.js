9bbbcff47657eef164068532b90a7d295888c5e9
Dellucifer <pg99285@gmail.com>
2022-10-18 20:07:10 +0530

    Added components

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <div class="navbar">
+        <div class="ul">
+            <a href="#">Home</a>
+        </div>
+        <div class="ul">
+            <a href="#">Team</a>
+        </div>
+        <div class="ul">
+            <a href="#">Projects</a>
+        </div>
+        <div class="ul">
+            <a href="#">About</a>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/TextBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TextBox() {
+  return (
+    <div>
+      <div class="box">
+        <p class="text-in-box">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis architecto velit nobis, corrupti dolore magnam exercitationem eligendi minus molestias beatae maxime ullam sapiente quisquam quas consequuntur eos quasi atque autem.
+        Unde inventore, quia eligendi velit quod voluptatum perspiciatis sit itaque minus cum, sapiente quos consectetur optio veritatis! Eos quod ullam assumenda? Cupiditate ad saepe ratione nemo aspernatur quia explicabo blanditiis.
+        Autem architecto sunt sint possimus non aperiam nobis nemo veritatis. Nihil, possimus. Suscipit blanditiis deleniti tempore commodi recusandae cupiditate fugiat exercitationem quod, perspiciatis nesciunt magnam. Quam deleniti eveniet delectus enim.
+        Dolore accusantium a tempore numquam veniam mollitia impedit officiis est corrupti, ex alias sequi quam ipsum deserunt facilis eveniet eius itaque sit excepturi. Atque odit commodi deserunt ullam, voluptates ipsam?</p>
+    </div>
+    </div>
+  )
+}
+
+export default TextBox
