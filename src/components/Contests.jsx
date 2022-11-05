ddbde0f65839135e93fdfbf375c6fae1a3ce098b
Dellucifer <pg99285@gmail.com>
2022-11-05 22:31:56 +0530

    Added react reveal and stylings

--- src/components/Contests.jsx
+++ src/components/Contests.jsx
@@ -1,39 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import Slide from 'react-reveal/Slide';
 
 const Contests = () => {
-    const [contests, setContests] = useState([]);
-    console.log(contests);
+  const [contests, setContests] = useState([]);
+  console.log(contests);
 
-    useEffect(() => {
-        const getContests = async (siteName) => {
-            await fetch("https://kontests.net/api/v1/"+siteName)
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                const cntst = data.map((contest) => ({
-                    name: contest.name,
-                    link: contest.url
-                }));
-                setContests(cntst);
-            });
-        };
-        getContests('codeforces');
-      }, []);
+  useEffect(() => {
+    const getContests = async (siteName) => {
+      await fetch("https://kontests.net/api/v1/" + siteName)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          const cntst = data.map((contest) => ({
+            name: contest.name,
+            link: contest.url,
+          }));
+          setContests(cntst);
+        });
+    };
+    getContests("codeforces");
+  }, []);
 
   return (
-    <div>
-      {contests.map(({ name, link }) => {
-        return (
-          <tr key={name}>
-            {/* <td>{name}</td> */}
-            <td>
-              <strong><a href={link} target="_blank" rel="noreferrer">{name}</a></strong>
-            </td>
-          </tr>
-        );
-      })}
+    <Slide left>
+    <div className="d-flex justify-content-center">
+      <div>
+        {contests.map(({ name, link }) => {
+          return (
+            <tr key={link}>
+              <td>
+                <strong>
+                  <a href={link} target="_blank" rel="noreferrer">
+                    {name}
+                  </a>
+                </strong>
+              </td>
+            </tr>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+    </Slide>
+  );
+};
 
-export default Contests
+export default Contests;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
     return (
-        <div>
+        <div className="footer">
             <footer>
+                <img
+                    className="footer__logo"
+                    src={'https://raw.githubusercontent.com/dellucifer/ssdc-web-dev/master/public/ssdc.jpeg'}
+                    alt="Logo"
+                />
                 <center>MAde by hardComder</center>
             </footer>
         </div>
