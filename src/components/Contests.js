bdada81acbd5c4f38f86e3241a52671ee5b88197
Dellucifer <pg99285@gmail.com>
2022-11-19 22:17:08 +0530

    Added BG

--- src/components/Contests.js
+++ src/components/Contests.js
@@ -4,14 +4,14 @@ import "./style.css";
 
 const Contests = () => {
   const [contests, setContests] = useState([]);
-  console.log(contests);
+  // console.log(contests);
 
   useEffect(() => {
     const getContests = async (siteName) => {
       await fetch("https://kontests.net/api/v1/" + siteName)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
+          // console.log(data);
           const cntst = data.map((contest) => ({
             name: contest.name,
             link: contest.url,
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,18 +7,14 @@ import './style.css'
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
-
-      <div className="SSDC">
+      <div className="SSDC bg">
+        <Navbar />
         <h1 className='text-center display-1'>SSDC</h1>
         <h3 className='text-center display-5'>Hive of Developers</h3>
+        <TextBox />
+        <Contests className="my-3"/>
+        <Footer />
       </div>
-
-      <TextBox />
-      <Contests className="my-3"/>
-      <Footer />
-    </div>
   )
 }
 
